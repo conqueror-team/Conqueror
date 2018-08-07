a5b799837abc2ca96755ddae1cb4c8a1e268864c
xiangwei <1050676672@qq.com>
2018-08-07 16:23:42 +0800

    配置文件提交

--- src/main/java/com/conqueror/blacklist/BootApplication.java
+++ src/main/java/com/conqueror/blacklist/BootApplication.java
@@ -1,9 +1,9 @@
 package com.conqueror.blacklist;
 
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({ "com.conqueror.blacklist"})
